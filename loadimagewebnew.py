a99b5764791de3273c868f3e91dde81f5f1ab8c3
VLx <vlxmenblack@gmail.com>
2025-05-03 14:24:25 +0530

    changes

--- loadimagewebnew.py
+++ loadimagewebnew.py
@@ -64,4 +64,5 @@ def predict():
     return jsonify({"device_name": device_name, "status": result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
